a34013d54a0bcb052336252a82663eb4dab2b8c9
julia-finamore <juliamm_finamore@hotmail.com>
2023-01-20 10:59:32 -0300

    Codei a fórmula que retorna a conversão correta

---
+++ app.js
@@ -0,0 +1,6 @@
+// Perguntar a temperatura em Celsius
+let temperature = prompt("Qual a temperatura do momento, em Celsius")
+// Converter a temperatura para Farenheit
+let convertToFarenheit = (temperature*1.8) + 32;
+// Mostrar no site
+document.write("A temperatura em Farenheit é: "+ convertToFarenheit);
